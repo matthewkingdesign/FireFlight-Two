b1cb13fa195f10509fb90b2c4889bba340db426c
matthewkingdesign <hello@matthewking.design>
2020-07-31 11:37:41 +0100

    adeed vimeo video to homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,5 +1,9 @@
 <?php snippet('header-home') ?>
 <div class="content-home">
+  <div class="vimeo-wrapper">
+     <iframe src="https://player.vimeo.com/video/443077884?background=1&autoplay=1&loop=1&byline=0&title=0&muted=1"
+             frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+  </div>
     <?php
       $images =  $page->menuicon()->toFiles();
       foreach($images as $image): ?>
